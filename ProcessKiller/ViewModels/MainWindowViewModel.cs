542c9f66ad2b2e67cadf34542b0fab0e0f979b14
Infarh <shmachilin@yandex.ru>
2021-08-16 10:29:18 +0300

    Модель-представления

---
+++ ProcessKiller/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using ProcessKiller.ViewModels.Base;
+
+namespace ProcessKiller.ViewModels
+{
+    public class MainWindowViewModel : ViewModel
+    {
+        #region Title : string - Заголовок окна
+
+        /// <summary>Заголовок окна</summary>
+        private string _Title = "Процессы";
+
+        /// <summary>Заголовок окна</summary>
+        public string Title { get => _Title; set => Set(ref _Title, value); }
+
+        #endregion
+    }
+}
